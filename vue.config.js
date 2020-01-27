7b50f837e3ec4ecb06c9343f5978ab395aeb8355
772809720@qq.com <772809720@qq.com>
2020-01-27 15:54:38 +0800

    完成项目初始化，首页头部样式修改

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+// vue.config.js
+const path = require('path')
+const webpack = require('webpack')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+
+
+module.exports = {
+    // 选项...
+    chainWebpack: config => {
+        config.resolve.alias
+          // key,value自行定义，比如.set('@assets', resolve('src/assets'))
+          .set('styles',resolve('src/assets/styles'))
+      }
+    
+  }
