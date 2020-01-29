7f01462c9790fd2c4e8018746ef85328da847995
772809720@qq.com <772809720@qq.com>
2020-01-29 23:10:41 +0800

    city页面基本开发完成

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,15 +1,50 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
+
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
+
+
 export default {
     name: 'city',
     components: {
-        CityHeader
+        CityHeader,
+        CitySearch,
+        CityList,
+        CityAlphabet
+    },
+    data(){
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    methods: {
+        getCityInfo(){
+            axios.get('/api/city.json').then(res => {
+                res = res.data
+                if(res.ret && res.data){
+                    const data = res.data
+                    this.cities = data.cities
+                    this.hotCities = data.hotCities
+                }
+                console.log(res)
+            })
+        }
+    },
+    mounted () {
+        this.getCityInfo()
     }
 }
 </script>
